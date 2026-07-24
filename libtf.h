c5ac1b0525d68cd5a017ac4231416a3c82b9e45d
Lucas <llinsbrz@gmail.com>
2026-07-24 12:26:46 +0200

    ft_isdigit done.

--- libtf.h
+++ libtf.h
@@ -24,3 +24,17 @@ int	ft_isalpha(char *str)
 	}
 	return (1);
 }
+
+int	ft_isdigit(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (!(str[i] >= '0' && str[i] <= '9'))
+			return (0);
+		i++;
+	}
+	return (1);
+}
