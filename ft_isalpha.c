c61cf5a74a6a7fbcbe1fd8228045324a9e936e31
Lucas <llinsbrz@gmail.com>
2026-07-27 19:23:14 +0200

    changes

---
+++ ft_isalpha.c
@@ -0,0 +1,9 @@
+int	ft_isalpha(int c)
+{
+	if (!((c >= 'A' && c <= 'Z')
+			|| (str[i] >= 'a' && str[i] <= 'z')))
+	{
+		return (0);
+	}
+	return (1);
+}
--- libtf.h
+++ libtf.h
@@ -10,58 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isalpha(int c)
-{
-	if (!((c >= 'A' && c <= 'Z')
-			|| (str[i] >= 'a' && str[i] <= 'z')))
-	{
-		return (0);
-	}
-	return (1);
-}
 
-int	ft_isdigit(int c)
-{
-	if (!(c >= '0' && c <= '9'))
-	{
-		return (0);
-	}
-	return (1);
-}
 
-int	ft_isalnum(int c)
-{
-	if (!(c >= '0' && c <= '9')
-		|| (c >= 'a' && c <= 'z')
-		|| (c >= 'A' && c <= 'Z'))
-	{
-		return (0);
-	}
-	return (1);
-}
 
-int	ft_isascii(int c)
-{
-	if (!(c >= 0 && c <= 127))
-		return (0);
-	return (1);
-}
 
-int	ft_isprint(int c)
-{
-	if (!(c >= 32 && <= 126))
-		return (0);
-	return (1);
-}
 
-int	ft_strlen(char *str)
-{
-	int	i;
 
-	i = 0;
-	while (str[i])
-	{
-		i++;
-	}
-	return (i);
-}
+
+
+
+
+
+
+int	
